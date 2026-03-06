88612a8425f438fec0531360468c824dcb3b998c
jn-gautier <jean.nicolas.gautier@gmail.com>
2026-03-06 15:22:17 +0100

    Update pendu.py

--- pendu.py
+++ pendu.py
@@ -1,6 +1,13 @@
 mot="anniversaire"
+list_lettres_mot=[]
+for lettre in mot:
+    if lettre not in list_lettres_mot:
+        list_lettres_mot.append(lettre)
+tentatives_autorisees=len(list_lettres_mot)+3
+
+
 list_propositions=[]
-for i in range(5):
+for i in range(tentatives_autorisees):
     for lettre in mot:
         if lettre in list_propositions :
             print( lettre,end="")
@@ -9,4 +16,15 @@ for i in range(5):
     print("")
     proposition=input("Faites une proposition : ")
     list_propositions.append(proposition)
+    if len(proposition)>1:
+        if proposition==mot:
+            print("Bravo, vous avez gagné WMWMWMW !")
+            print(f"Le mot était bien : {mot}")
+            break
+    
+    if set(list_propositions).intersection(set(list_lettres_mot))==set(list_lettres_mot):
+        print("Bravo, vous avez gagné WMWMWMW !")
+        print(f"Le mot était bien : {mot}")
+        break
+
 
